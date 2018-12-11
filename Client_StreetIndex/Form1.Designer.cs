45a352918b18e7220476d947c53fcd601a8ee66e
Makc <mks59k@gmail.com>
2018-12-11 21:11:08 +0200

    Визуальное оформление клиента.

--- Client_StreetIndex/Form1.Designer.cs
+++ Client_StreetIndex/Form1.Designer.cs
@@ -28,20 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBoxPostCode = new System.Windows.Forms.MaskedTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBoxStreets = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
+            // 
+            // textBoxPostCode
+            // 
+            this.textBoxPostCode.Location = new System.Drawing.Point(12, 12);
+            this.textBoxPostCode.Mask = "00000";
+            this.textBoxPostCode.Name = "textBoxPostCode";
+            this.textBoxPostCode.Size = new System.Drawing.Size(100, 21);
+            this.textBoxPostCode.TabIndex = 0;
+            this.textBoxPostCode.ValidatingType = typeof(int);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(146, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // listBoxStreets
+            // 
+            this.listBoxStreets.FormattingEnabled = true;
+            this.listBoxStreets.Location = new System.Drawing.Point(13, 40);
+            this.listBoxStreets.Name = "listBoxStreets";
+            this.listBoxStreets.Size = new System.Drawing.Size(208, 121);
+            this.listBoxStreets.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(450, 293);
+            this.ClientSize = new System.Drawing.Size(285, 172);
+            this.Controls.Add(this.listBoxStreets);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxPostCode);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MaskedTextBox textBoxPostCode;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBoxStreets;
     }
 }
 
--- Server_StreetIndex/Program.cs
+++ Server_StreetIndex/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,63 @@ namespace Server_StreetIndex
 {
     class Program
     {
+        static int port = 8000;
+
         static void Main(string[] args)
         {
+
+            Socket socketServer = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.IP);
+
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, port);
+
+
+            try
+            {
+                // Связываем сокет с локальной точкой,
+                // по которой будем принимать данные.
+                socketServer.Bind(ipEndPoint);
+
+                // Начинаем прослушивание.
+                socketServer.Listen(10);
+
+                Console.WriteLine("\nСервер запущен.\n");
+
+
+                while (true)
+                {
+                    Socket socketClient = socketServer.Accept();
+
+
+                    Console.WriteLine("Client endPoint: " +
+                        socketClient.RemoteEndPoint.ToString());
+                    Console.WriteLine("socketClient.ReceiveBufferSize: " 
+                        + socketClient.ReceiveBufferSize.ToString());
+                    Console.WriteLine("socketClient.Available: "
+                        + socketClient.Available.ToString());
+                    // Получаем сообщение.
+                    StringBuilder stringBuilder = new StringBuilder();
+                    int bytes = 0;  // Кол-во полученных байт.
+                    // Буфер для получаемых данных.
+                    byte[] data = new byte[socketClient.ReceiveBufferSize];
+
+                    do
+                    {
+                        // TODO
+
+                    } while (socketClient.Available > 0);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\n\n[error] " + ex.Message);
+            }
+
+
         }
     }
 }
